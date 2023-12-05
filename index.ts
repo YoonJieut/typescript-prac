b447c15352ef3b2f499b5b20740fb03e6a06b104
YoonJieut <qmdldos@gmail.com>
2023-12-06 00:01:04 +0900

    feat : createComponent 함수 작성 및 주석 추가 완료

--- index.ts
+++ index.ts
@@ -11,13 +11,38 @@ interface Props {
 function createComponent(element : string, props:Props, children?: string[]):string {
   // ? 이건 뭘까? 배열안에 굳이 넣은 이유는 뭐지?
   let tagParts = [`<${element}`];
+
   //? Partial은 또 무슨 뜻일까? 뒤는 css스타일을 참고하는 거 같은데..?
   const styleToString = (style: Partial<CSSStyleDeclaration>):string => {
-    //? entries()메서드는 뭐였지?
+    //? entries()메서드는 뭐였지? 맞다, [키,value] 배열을 반환하는 메서드였다.
     const entries = Object.entries(style);
     const objectValue = entries.map(([key, value])=> `${key} : ${value}`);
     const result = objectValue.join(' ');
     return result
   }
+  // 
+  for (const [key, value] of Object.entries(props)) {
+    let attributeString;
+    // ? 여기서 value의 타입이 왜 "object이지?"
+    if(key === "style" && typeof(value) === "object"){
+      attributeString = `style = "${styleToString(value)}"`;
+    } else {
+      attributeString = `${key} : "${value}"`;
+    }
+    tagParts.push(attributeString);
+  }
+  // 태그를 닫는다.
+  tagParts.push('>');
+
+  if(children){
+    tagParts.push(`</${element}>`);
+
+    const result = tagParts.join('');
+    return result;
+  }
+
+
+
 
 }
+
