454dea6efe3bd4ff192c0e5f92c6ca835d4e63cb
YoonJieut <qmdldos@gmail.com>
2023-12-21 23:37:33 +0900

    clear : 파일 청소

--- typescript-3/styleToString.ts
+++
@@ -1,5 +0,0 @@
-export const styleToString = style =>{
-  const entries = Object. entries(style);
-  const objectValue = entries.map(([key, value])=>`${key}: ${value};`);
-  return objectValue.join(' ');
-}
