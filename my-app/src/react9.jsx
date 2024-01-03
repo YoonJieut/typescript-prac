94dc6b9d5ec930cfc7856abf99ca884d1e5a6d6a
YoonJieut <qmdldos@gmail.com>
2024-01-04 00:13:54 +0900

    feat : jsx 타입으로 작성해본다.

---
+++ my-app/src/react9.jsx
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+function ReactComponent(props){
+  const Tag = props.tagName;
+  const { tagName, ...otherProps } = props;
+  return <Tag {...otherProps} />;
+}
+
+export default ReactComponent;
