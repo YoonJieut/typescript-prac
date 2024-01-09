05795f200488f171a4037356060188130314cbd4
YoonJieut <qmdldos@gmail.com>
2024-01-10 00:23:31 +0900

    feat : 팩 추가하는 함수 추가

--- my-app/src/react10-2.jsx
+++ my-app/src/react10-2.jsx
@@ -21,10 +21,13 @@ export function ReactTenDashOne() {
     // 구조분해할당으로 "할당"받았으며, 1번째(두번째)는 React에 의해 이미 만들어진 함수(메서드)이다.
   };
 
+  const addPack = () => {
+    setPacks([...packs, false]); // packs를 복사하고, false를 추가한다.
+    // 즉, 배열길이가 늘어남에 따라, packs 배열의 원소가 늘어난다.
+  };
+
     return (
-        <div>
-            <h1>React 10-2</h1>
-            <p>React 10-2</p>
-        </div>
+      <div>
+      </div>
     )
 }
